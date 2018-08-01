ab90d6a86fcbf96e2e3779c3c803961bc0d44e26
Tyshchenko Andrey <ggoldenfeniks@gmail.com>
2018-08-01 23:27:12 +1000

    Add Dormand-Prince coefficients

--- runge_kutta.hpp
+++ runge_kutta.hpp
@@ -102,6 +102,67 @@ namespace isope {
         runge_coefficients() : coefficients<T, 4>(a_table, b_table, c_table) {}
     };
 
+    template<typename T>
+    class dormand_prince_coefficients1 : public coefficients<T, 7> {
+
+    public:
+
+        static constexpr types::array2d_t<T, 7> a_table =
+                {
+                        T(0),                T(0),               T(0),                T(0),             T(0),               T(0),          T(0),
+                        T(1)     / T(5),     T(0),               T(0),                T(0),             T(0),               T(0),          T(0),
+                        T(3)     / T(40),    T(3)     / T(40),   T(0),                T(0),             T(0),               T(0),          T(0),
+                        T(44)    / T(45),   -T(56)    / T(15),   T(32)    / T(9),     T(0),             T(0),               T(0),          T(0),
+                        T(19372) / T(6561), -T(25360) / T(2187), T(64448) / T(6561), -T(212) / T(729),  T(0),               T(0),          T(0),
+                        T(9017)  / T(3168), -T(355)   / T(33),   T(46732) / T(5247),  T(49)  / T(176), -T(5103) / T(18656), T(0),          T(0),
+                        T(35)    / T(384),   T(0),               T(500)   / T(1113),  T(125) / T(192), -T(2187) / T(6784),  T(11) / T(84), T(0),
+                };
+        static constexpr types::array1d_t<T, 7> b_table =
+                {
+                         T(35)   / T(384),
+                         T(0),
+                         T(500)  / T(1113),
+                         T(125)  / T(192),
+                        -T(2187) / T(6784),
+                         T(11)   / T(84),
+                         T(0)
+                };
+        static constexpr types::array1d_t<T, 7> c_table =
+                {
+                        T(0),
+                        T(1) / T(5),
+                        T(3) / T(10),
+                        T(4) / T(5),
+                        T(8) / T(9),
+                        T(1),
+                        T(1)
+                };
+
+        dormand_prince_coefficients1() : coefficients<T, 7>(a_table, b_table, c_table) {}
+    };
+
+    template<typename T>
+    class dormand_prince_coefficients2 : public coefficients<T, 7> {
+
+    public:
+
+        static constexpr types::array2d_t<T, 7> a_table = dormand_prince_coefficients1<T>::a_table;
+        static constexpr types::array1d_t<T, 7> b_table =
+                {
+                         T(5179)  / T(57600),
+                         T(0),
+                         T(7571)  / T(16695),
+                         T(393)   / T(640),
+                        -T(92097) / T(339200),
+                         T(187)   / T(2100),
+                         T(1) / T(40)
+                };
+        static constexpr types::array1d_t<T, 7> c_table = dormand_prince_coefficients1<T>::c_table;
+
+        dormand_prince_coefficients2() : coefficients<T, 7>(a_table, b_table, c_table) {}
+
+    };
+
     template<typename ValueType, typename ArgumentType, typename Coeffs = runge_coefficients<ValueType, 4>>
     class runge_kutta {};
 
